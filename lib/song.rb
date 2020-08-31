4975a62b3524efb835988a48be0ec6c737618f8b
Luana Leal <luana.leal.eq@gmail.com>
2020-08-31 02:42:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,6 +20,7 @@ class Song
     arr = file_name.split("-").map(&:strip)
     artist = Artist.new(arr[0])
     song = self.new(arr[1])
+    artist.add_song(song)
     song.artist = artist
     song
   end 
