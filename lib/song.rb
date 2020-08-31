fbc85ed71d6e0360dcdff87c6b73a61591ace3b6
Luana Leal <luana.leal.eq@gmail.com>
2020-08-31 02:38:04 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song 
+  
+  attr_accessor :name
+  attr_reader :artist 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+ 
+  def self.all 
+    @@all 
+  end 
+  
+  
+  def self.new_by_filename(file_name)
+    arr = file_name.split("-")
+    artist = Artist.new(arr[0])
+    song = self.new(arr[1])
+    #song.artist = artist
+  end 
+  
+ 
+  def artist=(artist) 
+    @artist = artist
+  end 
+  
+  def artist_name=(name) 
+  #Song.all = Artist.all.find {|artist| artist.name == name})
+  end 
+  
+end 
+
+
+
