82c1eafc0ac12a61c8a4708b47ae052a843dd75b
Luana Leal <luana.leal.eq@gmail.com>
2020-08-31 02:39:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
   
   
   def self.new_by_filename(file_name)
-    arr = file_name.split("-")
+    arr = file_name.split("-").map(&:strip)
     artist = Artist.new(arr[0])
     song = self.new(arr[1])
     #song.artist = artist
